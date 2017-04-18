dec72bb977425cd8149f38faa26af62b12c5572b
Dylan Winn-Brown <dylan@winn-brown.co.uk>
2017-04-18 18:04:33 +0100

    Page transitions and node module ignored

--- inc/js/functions.js
+++ inc/js/functions.js
@@ -1,5 +1,17 @@
 jQuery(document).ready(function () {
-   if (jQuery) {
-       console.log("jQuery Loaded");
-   }
+   jQuery(".site-content > .container > main").css("display", "none");
+   jQuery("footer").css("display", "none");
+   jQuery(".site-content > .container > main").fadeIn(1000);
+   jQuery("footer").fadeIn(1000);
+   jQuery(".right-nav ul li a").click(function(event) {
+       event.preventDefault();
+       linkLocation = this.href;
+       jQuery(".site-content > .container > main").fadeOut(1000, redirectPage);
+       jQuery("footer").fadeOut(1000, redirectPage);
+   });
+
+   function redirectPage() {
+        window.location = linkLocation;
+    }
+
 });
