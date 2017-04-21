5be50f4077f9ff658827f0b4e9e351af963766df
Dylan Winn-Brown <dylan@winn-brown.co.uk>
2017-04-21 09:31:40 +0100

    Faster page transitions

--- inc/js/functions.js
+++ inc/js/functions.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function () {
    jQuery(".site-content > .container > main").css("display", "none");
    jQuery("footer").css("display", "none");
-   jQuery(".site-content > .container > main").fadeIn(1000);
-   jQuery("footer").fadeIn(1000);
+   jQuery(".site-content > .container > main").fadeIn(500);
+   jQuery("footer").fadeIn(500);
    jQuery(".right-nav ul li a").click(function(event) {
        event.preventDefault();
        linkLocation = this.href;
-       jQuery(".site-content > .container > main").fadeOut(1000, redirectPage);
-       jQuery("footer").fadeOut(1000, redirectPage);
+       jQuery(".site-content > .container > main").fadeOut(500, redirectPage);
+       jQuery("footer").fadeOut(500, redirectPage);
    });
 
    function redirectPage() {
